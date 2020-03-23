bb6df3562927b353dcb3f4b202e6b234c3a8057a
Nomur <Ikkai1000@hotmail.fr>
2020-03-23 14:09:47 +0100

    petits changements

--- src/app/affichage/pages/page-suggestion/page-suggestion.component.ts
+++ src/app/affichage/pages/page-suggestion/page-suggestion.component.ts
@@ -8,39 +8,48 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class PageSuggestionComponent implements OnInit {
 
-  suggestForm: FormGroup;
+  suggestFormEntreprise: FormGroup;
+  suggestFormService: FormGroup;
   pagesupp: boolean = true;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.suggestForm = this.fb.group({  // Crée une instance de FormGroup
+    this.suggestFormEntreprise = this.fb.group({  // Crée une instance de FormGroup
       nom: ['', [Validators.minLength(8), Validators.required]],                   // Crée une instance de FormControl
-      siren: [],                   // Crée une instance de FormControl
-      siret: [],                   // Crée une instance de FormControl
-      adresse: [],                   // Crée une instance de FormControl
-      representant: [],                   // Crée une instance de FormControl
-      email: ['', Validators.email],                   // Crée une instance de FormControl
-      description: [],                   // Crée une instance de FormControl
-      telephone: ['', Validators.pattern('[0-9]*')],                   // Crée une instance de FormControl
-      ville: [],                   // Crée une instance de FormControl
-      codePostal: [],                   // Crée une instance de FormControl
-      service: [''],                   // Crée une instance de FormControl
-      zoneIntervention: [''],                   // Crée une instance de FormControl
-      URLService: [''],                   // Crée une instance de FormControl
-      numeroRegistre: [''],                   // Crée une instance de FormControl
+      siren: [],                   
+      siret: [],                                      
+      representant: [],                   
+      email: ['', Validators.email],   
+      telephone: ['', Validators.pattern('[0-9]*')],                
+      description: [],                   
+      adresse: [],                   
+      ville: [],                  
+      codePostal: [],                   
+      service: [''],                   
+      zoneIntervention: [''],                                      
+    });
+    this.suggestFormService = this.fb.group({  // Crée une instance de FormGroup
+      URLService: ['', [Validators.minLength(4), Validators.required]],
+      nom: ['', [Validators.minLength(8), Validators.required]],                   // Crée une instance de FormControl                                   
+      representant: [],                                     
+      email: ['', Validators.email],                    
+      telephone: ['', Validators.pattern('[0-9]*')],  
+      description: [],                                    
+      service: [''],                                                        
+      numeroRegistre: [''],                   
     });
   }
 
   suggestEntreprise() {
-    console.log('Données du formulaire...', this.suggestForm.get('nom').value);
-    console.log('Données du formulaire...', this.suggestForm.value);
+    console.log('Données du formulaire...', this.suggestFormEntreprise.get('nom').value);
+    console.log('Données du formulaire...', this.suggestFormEntreprise.value);
     this.pagesupp = false;
   }
 
   suggestSW() {
-    console.log('Données du formulaire...', this.suggestForm.get('nom').value);
-    console.log('Données du formulaire...', this.suggestForm.value);
+    console.log('Données du formulaire...', this.suggestFormService.get('nom').value);
+    console.log('Données du formulaire...', this.suggestFormService.value);
     this.pagesupp = false;
   }
 
